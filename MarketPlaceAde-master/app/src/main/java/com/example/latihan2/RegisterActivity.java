6bb9a42d7fb2572962ee1ed68d450f97412ac144
Faurizaa <faurizafarhan@gmail.com>
2020-02-20 22:10:53 +0700

    Initial commit

---
+++ MarketPlaceAde-master/app/src/main/java/com/example/latihan2/RegisterActivity.java
@@ -0,0 +1,33 @@
+package com.example.latihan2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.animation.TypeEvaluator;
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class RegisterActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+    }
+
+    public void back(View view) {
+        Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+
+        startActivity(intent);
+    }
+
+    public void nextProfile(View view) {
+        Intent intent = new Intent(RegisterActivity.this, ProfileActivity.class);
+
+        startActivity(intent);
+    }
+}
